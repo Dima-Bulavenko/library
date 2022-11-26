5e933e3e919c9b12084cb46cd8bfa0d0f4127961
DianaUzun <dianauzun2006@gmail.com>
2022-11-26 02:27:28 +0200

    add fields to user/ and author/ serializers; add method to order model which allows to select orders by user

--- library/authentication/serializers.py
+++ library/authentication/serializers.py
@@ -5,4 +5,5 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['url', 'id', 'first_name', 'last_name', 'middle_name', 'email', 'role', 'password', 'created_at',
+                  'updated_at', 'is_superuser', 'is_staff', 'is_active']
