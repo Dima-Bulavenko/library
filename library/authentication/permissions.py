57f797b2169450750d7a504015a1ac62b991a384
Dima Bulavenko <dima99bylovenko@gmail.com>
2022-11-28 21:07:54 +0000

    permissions for order

--- library/authentication/permissions.py
+++ library/authentication/permissions.py
@@ -8,33 +8,36 @@ class CustomUserPermission(permissions.BasePermission):
                 return True
 
 
-class IsSuperUserOrNotAuthenticate(permissions.BasePermission):
-    # def has_permission(self, request, view):
+class IsSuperUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+    # def has_object_permission(self, request, view, obj):
     #     print(not request.user.is_authenticated)
     #     if not request.user.is_authenticated or request.user.is_superuser:
     #         return True
     #
     #     return False
 
-    def has_object_permission(self, request, view, obj):
-        print(not request.user.is_authenticated)
-        if not request.user.is_authenticated or request.user.is_superuser:
-            return True
-
-        return False
-
 
 class IsOwnerOrStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        print(request.user.is_staff or request.user == obj)
-        return request.user.is_staff or request.user == obj
+        # print(request.user.is_staff or request.user == obj)
+        return request.user.is_staff or request.user == obj or request.user.role != 0
 
 
 class IsOwnerOrSuperUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        # print(request.user.is_superuser or request.user == obj)
         return request.user.is_superuser or request.user == obj
 
 
 class IsNotAllowed(permissions.BasePermission):
     def has_permission(self, request, view):
         return False
+
+
+class NotAuthenticate(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(not request.user.is_authenticated)
+        return not request.user.is_authenticated
